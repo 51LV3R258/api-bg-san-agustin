659b4769804323ab2cfb1866c81233dbe6812c61
51lv3r258 <jrp8900@gmail.com>
2020-07-10 18:25:03 -0500

    :card_file_box: nueva tabla sales y detail_sales

---
+++ database/migrations/2020_07_10_175401_create_detail_sales.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailSales extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_sales', function (Blueprint $table) {
+            $table->foreignId('sale_id')->constrained()->onUpdate('cascade');
+            $table->foreignId('product_id')->constrained()->onUpdate('cascade');
+            $table->foreignId('unit_id')->constrained()->onUpdate('cascade');
+            $table->primary(['product_id', 'unit_id', 'sale_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_sales');
+    }
+}
